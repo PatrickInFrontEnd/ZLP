53824d4160d1c518f11bf32f590924aacd131909
PatrickInFrontEnd <patryk.pluciennik022@gmail.com>
2019-02-28 22:39:59 +0100

    Components folder has been added to the project
    Kubacomp - Kuba's styled-components
    Patrykcomp - Patryk's styled-components
    Mikołajcomp - Mikołaj's styled-components

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,13 @@
-import React  from 'react';
+import React from 'react';
+import Wrapper from '../Components/Patrykcomp/nav';
 
 class MainPage extends React.Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
             <p>Main Page</p>
         );
     }
 }
- 
+
 export default MainPage;
