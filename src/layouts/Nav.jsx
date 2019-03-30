78118634e8d9f892b9bba3167d56858b51d84ad4
Mikołaj <miki70707@interia.pl>
2019-03-30 01:45:02 +0100

    hamburgert animation has been added

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Colors } from "../Components/Colors";
-import { hamburgerAnimation } from './Hamburger';
 
 class Nav extends Component {
-  state = {};
+  state = {
+    isHamburgerClicked: false
+  };
+  handleClick = () => {
+    this.setState(prev => ({ isHamburgerClicked: !prev.isHamburgerClicked }));
+  };
   render() {
     return (
       <Navigation>
@@ -13,18 +17,55 @@ class Nav extends Component {
           <IMG src="Images/logo.png" alt="ZLP" />
         </NavLink>
         <NavSection>
-          <NavBtnStyle><NavLink to="/"><NavBtn>Strona Główna</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/blog"><NavBtn>Blog</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/drabinka"><NavBtn>Drabinka</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/regulamin"><NavBtn>Regulamin</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/plany"><NavBtn>Co w planach</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/rejestracja"><NavBtn isBig isWhite isBorder GreenHover>Zarejestruj się</NavBtn></NavLink></NavBtnStyle>
-          <NavBtnStyle><NavLink to="/logowanie"><NavBtn isBig isWhite isBorder GreenHover>Zaloguj się</NavBtn></NavLink></NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/">
+              <NavBtn>Strona Główna</NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/blog">
+              <NavBtn>Blog</NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/drabinka">
+              <NavBtn>Drabinka</NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/regulamin">
+              <NavBtn>Regulamin</NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/plany">
+              <NavBtn>Co w planach</NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/rejestracja">
+              <NavBtn isBig isWhite isBorder GreenHover>
+                Zarejestruj się
+              </NavBtn>
+            </NavLink>
+          </NavBtnStyle>
+          <NavBtnStyle>
+            <NavLink to="/logowanie">
+              <NavBtn isBig isWhite isBorder GreenHover>
+                Zaloguj się
+              </NavBtn>
+            </NavLink>
+          </NavBtnStyle>
         </NavSection>
-        <HamburgerBtn onClick={hamburgerAnimation}>
-          <Span></Span>
-          <Span></Span>
-          <Span></Span>
+        <HamburgerBtn
+          onClick={this.handleClick}
+          isClicked={this.state.isHamburgerClicked}
+        >
+          <Span isClicked={this.state.isHamburgerClicked} />
+          <Span isClicked={this.state.isHamburgerClicked} />
+          <Span isClicked={this.state.isHamburgerClicked} />
+          <Span isClicked={this.state.isHamburgerClicked} />
+          <Span isClicked={this.state.isHamburgerClicked} />
         </HamburgerBtn>
       </Navigation>
     );
@@ -36,103 +77,130 @@ export default Nav;
 /* note: Styled-Components section */
 
 const Navigation = styled.nav`
-      position:sticky;
-      left:0;
-      right:0;
-      top:0;
-      display:flex;
-      justify-items:center;
-      justify-content: space-around;
-      width:100%;
-      height:80px;
-      padding:0 20px;
-      background-color:#20202C;
-      border-bottom:2px solid #444;
-      z-index:10;
-      `;
-
+  position: sticky;
+  left: 0;
+  right: 0;
+  top: 0;
+  display: flex;
+  justify-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 80px;
+  padding: 0 20px;
+  background-color: #20202c;
+  border-bottom: 2px solid #444;
+  z-index: 10;
+`;
 
 const IMG = styled.img`
-      height:70px;
-      width:204px;
-      margin-top:5px;
-      `;
+  height: 70px;
+  width: 204px;
+  margin-top: 5px;
+`;
 
 const NavSection = styled.div`
-      display:flex;
-      justify-content:flex-end;
-      align-items:center;
-      width:80%;
-      height:100%;
-      `;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 80%;
+  height: 100%;
+`;
 
 const NavBtnStyle = styled.div`
-      display:flex;
-      text-align:center;
-      margin-left:20px;
-      position: relative;
+  display: flex;
+  text-align: center;
+  margin-left: 20px;
+  position: relative;
 
-      &::before{
-      content:'';
-      display:block;
-      position: absolute;
-      bottom: -5px;
-      left:0;
-      height: 2px;
-      width: 0%;
-      background-color: #fff;
-      transition: .2s ease-in-out all;
-      }
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    height: 2px;
+    width: 0%;
+    background-color: #fff;
+    transition: 0.2s ease-in-out all;
+  }
 
-      &:nth-of-type(6)::before,
-      &:nth-of-type(7)::before{
-        display:none;
-      }
+  &:nth-of-type(6)::before,
+  &:nth-of-type(7)::before {
+    display: none;
+  }
 
-      &:hover::before{
-        width:100%;
-      }
-      `;
+  &:hover::before {
+    width: 100%;
+  }
+`;
 
 export const NavBtn = styled.div`
-      position:relative;
-      padding:10px 15px;
-      font-family: 'Nunito','Montserrat','Arial','Sans-Serif';
-      font-size: ${(props) => props.isBig ? '22px' : '18px'};
-      font-weight:700;
-      color: ${Colors.blue_nav};
-      border: ${(props) => props.isBorder ? '2px solid' : 'none'};
-      border-radius:10px;
-      transition:.2s cubic-bezier(0.55, 0.055, 0.675, 0.19) all;
+  position: relative;
+  padding: 10px 15px;
+  font-family: "Nunito", "Montserrat", "Arial", "Sans-Serif";
+  font-size: ${props => (props.isBig ? "22px" : "18px")};
+  font-weight: 700;
+  color: ${Colors.blue_nav};
+  border: ${props => (props.isBorder ? "2px solid" : "none")};
+  border-radius: 10px;
+  transition: 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19) all;
 
-      &:hover{
-        color:#fff;
-        border-color:#1abc9c;
-      }
+  &:hover {
+    color: #fff;
+    border-color: #1abc9c;
+  }
 `;
 
 const HamburgerBtn = styled.div`
-    position: relative;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    justify-content:center;
-    top:10px;
-    width:60px;
-    height:60px;
-    padding:10px;
-    border-radius:50%;
-    background-color: ${Colors.bgColor};
-    cursor: pointer;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  top: 10px;
+  width: 60px;
+  height: 60px;
+  padding: 10px;
+  border-radius: 50%;
+  background-color: ${Colors.bgColor};
+  cursor: pointer;
 `;
 
 const Span = styled.span`
-    position: relative;
-    top:2px;
-    display:block;
-    width:100%;
-    height:5px;
-    margin:0 0 3px;
-    border-radius:2px;
-    background-color: #3a91ca;
+  position: relative;
+  top: 2px;
+  display: block;
+  width: 45px;
+  height: 5px;
+  margin: 0 0 6px;
+  border-radius: 2px;
+  background-color: #3a91ca;
+  &:nth-child(1) {
+    opacity: ${props => (props.isClicked ? 0 : 1)};
+    visibility: ${props => (props.isClicked ? "hidden" : "visible")};
+  }
+  &:nth-child(2) {
+    opacity: ${props => (props.isClicked ? 0 : 1)};
+    visibility: ${props => (props.isClicked ? "hidden" : "visible")};
+  }
+  &:nth-child(3) {
+    opacity: ${props => (props.isClicked ? 0 : 1)};
+    visibility: ${props => (props.isClicked ? "hidden" : "visible")};
+  }
+  &:nth-child(4) {
+    opacity: ${props => (props.isClicked ? 1 : 0)};
+    visibility: ${props => (props.isClicked ? "visible" : "hidden")};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
+  &:nth-child(5) {
+    opacity: ${props => (props.isClicked ? 1 : 0)};
+    visibility: ${props => (props.isClicked ? "visible" : "hidden")};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
 `;
