176374d13cd281d4934ef918daa668ed5cdb158e
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2019-03-31 13:27:13 +0200

    Cosmetic change of colors on the buttons/editing list game/remove border in "p"

--- src/Components/Colors.jsx
+++ src/Components/Colors.jsx
@@ -2,6 +2,7 @@ export const Colors = {
     white: '#f5f6fa',
     blue_text: '#1b9cfc',
     blue_nav: '#05A1E9',
-    green: '#1abc9c',
+    // green: '#1abc9c',
+    green: '#44bd32',
     bgColor: '#0e161d'
 };
